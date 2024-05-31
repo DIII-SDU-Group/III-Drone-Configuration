561cab01c63d6f5b81c9c9616eb54b98fc3ebb65
frnyb <ffn@mmmi.sdu.dk>
2024-05-31 17:01:49 +0000

    Installs scripts in python package so they can be imported from CLI (for starting configuration client)

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+
+package_name = 'iii_drone_configuration'
+description="The III-Drone configuration sub system. This package contains the configuration of the drone system, including configuration server node, parameter handler, parameter files, and C++ configurator classes."
+maintainer="Frederik Falk Nyboe"
+maintainer_email="ffn@sdu.dk"
+license="proprietary"
+version="2.2"
+
+setup(
+    name=package_name,
+    version=version,
+    packages=[package_name],
+    data_files=[
+        (os.path.join('share', package_name), ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    description=description,
+    license=license,
+    entry_points={
+        'console_scripts': [
+            "configuration_server = iii_drone_configuration.configuration_server_node:main",
+            "configuration_client = iii_drone_configuration.configuration_client_node:main",
+        ],
+    },
+)
